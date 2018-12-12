bdca314f6fd5a2be6bbae8b2e745cd0385e23a21
UTexas80 <glen.falk@utexas.edu>
2018-12-12 08:07:47 -0500

    20181212USG_TuiitionDifferentialA
    a. Update initial ProjectTemplate structure.

---
+++ munge/00-A.R
@@ -0,0 +1,9 @@
+
+ rm(list = ls()[grepl("(SQL|X2016Tuition)", ls())])    
+# rm(list = ls()[grepl("(SQL|Sheet|Export|X2016Tuition)", ls())])    
+
+#dataMeta package processing
+# variable_type <- c(1, 1)
+# variable_description <- c("DETAIL_CODE_DESC", "DEGREE.MAJOR")
+# linker <- build_linker(my.data = jctTbl.source, variable_description = variable_description, variable_type = variable_type)
+# metadata <- build_dict(my.data = jctTbl.source, linker = linker, option_description = NULL, prompt_varopts = FALSE)
